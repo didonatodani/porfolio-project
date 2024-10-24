836ecbbca8e50da6671a830a9978810d67f4f707
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-10-24 08:58:57 +0200

    about page finished

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ window.addEventListener("resize", () => {
         menuItems.classList.remove("visible");
     }
 });
+
+
+// PORTFOLIO PAGE
+
+const projectsArray = []
+
+const projectContainer = document.createElement("div")
+const projectImg = document.createElement("img")
+const projectTitle = document.createElement("h3")
+const projectDescription = document.createElement("p")
+const projectLink = document.createElement("a")
+
+
